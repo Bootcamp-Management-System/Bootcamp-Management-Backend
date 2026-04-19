852269e3694d015572b028a47d5d7385c076fa53
yumna08 <yumna1234h@gmail.com>
2026-04-19 17:08:05 +0300

    Revert "update on resource"
    This reverts commit 9fc902be2a3da7b50c1084c406441a9d9681d989.

--- src/routes/attendance.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { checkIn } from "../controllers/attendanceController.js";
-
-const router = express.Router();
-
-router.post("/check-in", checkIn);
-
-export default router;
