289d32da7af3b81568cb9dbe1627fd94f72025f5
yumna08 <yumna1234h@gmail.com>
2026-04-19 23:32:44 +0300

    check all routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import helmet from "helmet";
-import rateLimit from "express-rate-limit";
+// import helmet from "helmet";
+// import rateLimit from "express-rate-limit";
 import connectDB from "./config/db.js";
 
 import authRoutes from "./routes/auth.js";
@@ -18,7 +18,7 @@ connectDB();
 
 const app = express();
 
-app.use(helmet());
+// app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
@@ -26,13 +26,13 @@ app.use(express.json());
 import path from "path";
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
-const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 5,
-  message: "Too many login attempts, please try again after 15 minutes",
-});
+// const loginLimiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 5,
+//   message: "Too many login attempts, please try again after 15 minutes",
+// });
 
-app.use("/api/v1/auth/login", loginLimiter);
+
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/divisions", divisionRoutes);
